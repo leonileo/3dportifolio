4a9d39a685084038366cb496d9170702c7ac9aac
kaleb <leolittleprogrammer@gmail.com>
2024-03-26 12:49:44 +0300

    hot fix

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
+import { styles } from '../styles'
+import {navLinks } from '../constants'
+import {logo, menu, close} from '../assets'
+
+const Navbar = () => {
+  const [active, setActive] = useState('');
+  return (
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to='/' className='flex items-center gap-2' onClick={() => { setActive(""); window.scrollTo(0, 0)}}>
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+          <p>Kaleb <span className='sm:block hidden'>| Portfolio</span></p>
+        </Link>
+        <ul className='list-none hidden sm:flex text-white p-2 flex-row gap-10'>
+          {navLinks.map((links) => {
+            return <li key={links.id}>
+              <a href={`#${links.id}`}>{links.title}</a>
+            </li>
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
